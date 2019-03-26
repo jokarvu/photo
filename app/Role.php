2ea66bf07eaba9d3680cb3e79cc66da1cb9544d7
jokarvu <vuhoanghiep097@gmail.com>
2019-03-26 21:38:38 +0700

    Fixed some bugs

--- app/Role.php
+++ app/Role.php
@@ -5,7 +5,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
-{    
+{   
+    protected $fillable = ['name', 'description']; 
     public function users()
     {
         return $this->hasMany(User::class);
